c03a5a196a10f06705804548199e456ed4d1a89e
TKD04 <tkd04devpub@outlook.com>
2023-07-22 00:21:16 +0900

    Add supports for gif and svg

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,14 +1,22 @@
 import { deleteAsync } from "del";
 import GulpClient from "gulp";
-import imagemin from "gulp-imagemin";
+import imagemin, { gifsicle, mozjpeg, svgo } from "gulp-imagemin";
 import pngquant from "imagemin-pngquant";
 
 const { dest, src, series } = GulpClient;
 const gulpWatch = GulpClient.watch;
 
+// ref. https://designsupply-web.com/media/programming/4409/
 const minifyImages = () =>
   src("./src/*.{jpg,jpeg,png,gif,svg}")
-    .pipe(imagemin([pngquant({ quality: [0.5, 0.5] })]))
+    .pipe(
+      imagemin([
+        mozjpeg({ quality: 80 }),
+        pngquant({ quality: [0.65, 0.8] }),
+        svgo(),
+        gifsicle(),
+      ])
+    )
     .pipe(dest("./dist"));
 
 export const convert = () => minifyImages;
